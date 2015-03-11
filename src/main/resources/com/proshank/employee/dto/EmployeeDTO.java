da8f48807cd5b372f08ae45ea557b3f86fda1ea2
Pawankumar Roshankhede <proshank@cisco.com>
2015-03-11 22:07:03 +0530

    Updated for JPA cache

--- src/main/resources/com/proshank/employee/dto/EmployeeDTO.java
+++ src/main/resources/com/proshank/employee/dto/EmployeeDTO.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Entity
 @Table(name = "Employees")
 @Cacheable(true)
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+//@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class EmployeeDTO {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
